5e969a02e3f54591b34e7d9db5b4b8e1194c6b07
muhammad-huzaifah <huzaifah2009@gmail.com>
2022-07-04 15:46:00 +0700

    learn cookie

---
+++ 27.3.Cookie Deleted.php
@@ -0,0 +1,19 @@
+<?php
+// set the expiration date to one hour ago
+setcookie("user", "", time() - 3600);
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "Cookie 'user' is deleted."
+?>
+</body>
+</html>
