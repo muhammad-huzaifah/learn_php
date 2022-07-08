4b509b200a127363b5505c2171ab580c52a23205
muhammad-huzaifah <huzaifah2009@gmail.com>
2022-07-08 14:42:09 +0700

    learn json

---
+++ 31.7.Json Decode Loop Foreach.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $jsonobj = '{"Peter":35, "Ben":37, "Joe":43}';
+
+        $obj = json_decode($jsonobj);
+
+        foreach ($obj as $key => $value) {
+            echo $key . "=>" . $value . "<br>";
+        }
+    ?>
+</body>
+</html>
