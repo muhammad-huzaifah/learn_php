2d54bb52a18299e994ef755a0930d7d16663410c
muhammad-huzaifah <huzaifah2009@gmail.com>
2022-07-17 10:34:08 +0700

    static method

---
+++ 42.2.Static Method 2.php
@@ -0,0 +1,11 @@
+<?php
+class greeting {
+    public static function welcome() {
+        echo "Hello World!";
+    }
+    public function __construct(){
+        self::welcome();
+    }
+}
+
+new greeting();
